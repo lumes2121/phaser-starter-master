1143cf5b36b2d250bc2d94b1fe6b7d5faa180f16
henrique <jhcb007@gmail.com>
2017-06-13 00:33:16 -0300

    Trabalho 12

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
+var _texto_pontuacao;
+var _pontos = 0;
 var _palavras = [];
 
 function get_palavras() {
     $.getJSON("../palavras.json", function (data) {
-        console.log(data.palavras);
         _palavras = data.palavras;
     });
 }
@@ -23,32 +24,48 @@ function openDesafio() {
     });
 }
 
-function resposta() {
+function event_resposta() {
     game.paused = false;
-    var nome = $('#txtDesafio1').val();
-    if (nome == 'acerola') {
-        pontos += 1;
-        console.log('certo');
+    var resposta = {};
+    $('#fromDesavio').serializeArray().map(function (x) {
+        resposta[x.name] = x.value;
+    });
+    if (resposta.palavra === resposta.resposta) {
+        _pontos += 1;
+        swal({
+            type: "success",
+            title: "Palavra correta!!!",
+            text: "",
+            timer: 3000,
+            showConfirmButton: false
+        });
+    } else {
+        swal({
+            type: "error",
+            title: "Palavra errada.",
+            text: 'Palavra correta é: <span style="color:#10c469; font-weight: bold">' + resposta.palavra + '</span>',
+            timer: 4000,
+            showConfirmButton: false,
+            html: true
+        });
     }
-    console.log('errado');
-
     $('#modal1').modal('hide');
 }
 
 function popularModal() {
     var desafio = _palavras[Math.floor(Math.random() * _palavras.length)];
     var html = '';
-    console.log(desafio);
-    $("#desafioResposta").val(desafio.palavra);
+    $("#desafioPalavra").val(desafio.palavra);
     $("#labelImagem").attr('src', '../images/desavio/' + desafio.imagem);
     $.each(desafio.alternativas, function (key, value) {
         html += '<label class="custom-control custom-radio">';
-        html += '<input id="radio' + key + '" name="desafio" value="' + value + '" type="radio" class="custom-control-input">';
+        html += '<input id="radio' + key + '" name="resposta" value="' + value + '" type="radio" class="custom-control-input">';
         html += '<span class="custom-control-indicator"></span>';
         html += '<span class="custom-control-description">' + value + '</span>';
         html += '</label>';
     });
     $("#labelDesafio").html(html);
+    removePalavra(desafio);
 }
 
 
